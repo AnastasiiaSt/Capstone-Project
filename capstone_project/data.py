ac13521eb7653dd965a56862b652c79ffa631fd0
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-04 16:15:20 +0300

    The code divided in modules (functions)

--- capstone_project/data.py
+++ capstone_project/data.py
@@ -1,32 +1,39 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
-
-from plotnine import (
-    ggplot, aes, geom_point, geom_smooth, labs, theme_xkcd
-)
-
+import os
 
 # Read the datasets from the files
 
-train_set = pd.read_csv(r'C:\Users\anast\Documents\GitHub\Capstone-Project\data\train.csv')
-test_set = pd.read_csv(r'C:\Users\anast\Documents\GitHub\Capstone-Project\data\test.csv')
+path = r'C:\Users\anast\Documents\GitHub\Capstone-Project\data'
 
-print('Training dataset size:', train_set.shape)
-print('Training dataset columns:', train_set.columns.to_list())
+def get_data(path):
+    print('runnnnn')
+    file_name_train = 'train.csv'
+    file_name_test = 'test.csv'
+    file_train = os.path.join(path, file_name_train)
+    file_test = os.path.join(path, file_name_test)
+    train_set = pd.read_csv(file_train)
+    test_set = pd.read_csv(file_test)
 
-print('Test dataset size:', test_set.shape)
-print('Test dataset columns:', test_set.columns.to_list())
+    print('Training dataset size:', train_set.shape)
+    print('Training dataset columns:', train_set.columns.to_list())
+    print('Test dataset size:', test_set.shape)
+    print('Test dataset columns:', test_set.columns.to_list())
 
-train_y = train_set['Cover_Type']
-train_X = train_set.drop(['Cover_Type'], axis = 1)
+    train_y = train_set['Cover_Type']
+    train_X = train_set.drop(['Cover_Type'], axis = 1)
+    test_X = test_set
+    return train_X, train_y, test_X
 
-test_X = test_set
+get_data(path = path)
 
 # Perform EDA using pandas profiling library
 
-report_train = ProfileReport(train_set)
+def profile_report(dataset):
+    report = ProfileReport(dataset)
+    #report.to_file(r'C:\Users\anast\Documents\GitHub\Capstone-Project\capstone_project\profile_report_train.html')
+    return report
 
-report_train.to_file(r'C:\Users\anast\Documents\GitHub\Capstone-Project\capstone_project\profile_report_train.html')
 
 
 
