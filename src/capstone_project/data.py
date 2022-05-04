780246b72a609d3e770f2374e8e902926cb530bc
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-05 02:57:16 +0300

    Functions for data extraction and profiling report are created

--- src/capstone_project/data.py
+++ src/capstone_project/data.py
@@ -2,37 +2,32 @@ import pandas as pd
 from pandas_profiling import ProfileReport
 import os
 from pathlib import Path
+import click
 
 # Read the datasets from the files
 
-path = r'C:\Users\anast\Documents\GitHub\Capstone-Project\data'
+def get_data(path, file_name = 'train.csv'):
+    file_path = os.path.join(path, file_name)
+    dataset = pd.read_csv(file_path)
 
-def get_data(path):
-    file_name_train = 'train.csv'
-    file_name_test = 'test.csv'
-    file_train = os.path.join(path, file_name_train)
-    file_test = os.path.join(path, file_name_test)
-    train_set = pd.read_csv(file_train)
-    test_set = pd.read_csv(file_test)
+    click.echo('Dataset size: {0}.'.format(dataset.shape))
 
-    print('Training dataset size:', train_set.shape)
-    print('Training dataset columns:', train_set.columns.to_list())
-    print('Test dataset size:', test_set.shape)
-    print('Test dataset columns:', test_set.columns.to_list())
-
-    train_y = train_set['Cover_Type']
-    train_X = train_set.drop(['Cover_Type'], axis = 1)
-    test_X = test_set
-    return train_X, train_y, test_X
-
-get_data(path)
+    y = dataset['Cover_Type']
+    X = dataset.drop(['Cover_Type'], axis = 1)
+    return X, y
 
 # Perform EDA using pandas profiling library
 
-def profile_report(dataset):
+@click.command()
+@click.option('--dataset_path', default = os.path.join(Path.cwd(), 'data'))
+@click.option('--file_name', default = 'train.csv')
+@click.option('--path_to', default = Path.cwd())
+def profile_report(dataset_path, file_name, path_to):
+    file_path = os.path.join(dataset_path, file_name)
+    dataset = pd.read_csv(file_path)  
     report = ProfileReport(dataset)
-    #report.to_file(r'C:\Users\anast\Documents\GitHub\Capstone-Project\capstone_project\profile_report_train.html')
-    return report
+    path = os.path.join(path_to, 'profile_report.html')
+    report.to_file(path)
 
 
 
