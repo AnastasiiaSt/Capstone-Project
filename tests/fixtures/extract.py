ad59088fdcf368460df2b0bac1c3e3d9684bbb3c
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-10 22:17:42 +0300

    Update extract.py

--- tests/fixtures/extract.py
+++ tests/fixtures/extract.py
@@ -6,12 +6,12 @@ from sklearn.utils import shuffle
 
 def get_extract(input_path: Path = os.path.join(Path.cwd(), 'data'), output_path: Path = os.path.join(Path.cwd(), 'tests', 'fixtures'), input_file_name: str = 'train.csv', output_file_name: str = 'fixture.csv') -> pd.DataFrame:
     input_file_path = os.path.join(input_path, input_file_name)
-    dataset = pd.read_csv(input_file_path)
+    dataset = pd.read_csv(input_file_path, index_col = 'Id')
 
     dataset = shuffle(dataset, random_state = 42)
 
     output_file_path = os.path.join(output_path, output_file_name)
 
-    dataset[:99].to_csv(output_file_path)
+    dataset[:99].to_csv(output_file_path, index_label = 'Id', sep = ',')
 
 get_extract()
