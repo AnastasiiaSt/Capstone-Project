1687fda9cf3bd3dae77f69717389449208bf0343
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-10 22:13:56 +0300

    Update pipeline.py

--- src/capstone_project/pipeline.py
+++ src/capstone_project/pipeline.py
@@ -1,32 +1,51 @@
 import pandas as pd
-import os
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.compose import ColumnTransformer
-from pathlib import Path
-from data import get_data
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.decomposition import PCA
+from sklearn.impute import SimpleImputer
+from typing import Tuple
 
 
 # Create preprocessing pipeline
 
+def preprocess(dataset: pd.DataFrame, variance_threshold: bool, threshold: int, scaling: bool, pca: bool, n_components: int) -> Tuple[pd.DataFrame, dict]: 
 
-def preprocess(dataset):
+    params_prep = {}
 
     num_attribs = dataset.select_dtypes(include = ['number']).columns.to_list()
     cat_attribs = dataset.select_dtypes(include = ['category']).columns.to_list()
 
-    num_pipeline = Pipeline([
-        ('std_scaler', StandardScaler())
-    ])
+    num_pipeline_steps = []
+    cat_pipeline_steps = []
+
+    cat_pipeline_steps.append(('encoder', LabelEncoder()))
+    num_pipeline_steps.append(('imputer', SimpleImputer()))
+
+    if variance_threshold:
+        num_pipeline_steps.append(('variance_threshold', VarianceThreshold(threshold = threshold)))
+        params_prep['variance_threshold'] = True
+        params_prep['threshold'] = threshold
+
+    if pca:
+        num_pipeline_steps.append(('pca', PCA(n_components = n_components)))
+        params_prep['pca'] = True
+        params_prep['n_components'] = n_components
+
+    if scaling:
+        num_pipeline_steps.append(('scaling', StandardScaler()))
+        params_prep['scaling'] = True
 
     full_pipeline = ColumnTransformer([
-        ('numerical', num_pipeline, num_attribs),
-        ('categorical', LabelEncoder(), cat_attribs)
+        ('numerical', Pipeline(steps = num_pipeline_steps), num_attribs),
+        ('categorical', Pipeline(steps = cat_pipeline_steps), cat_attribs)
     ])
 
     preprocess_dataset = full_pipeline.fit_transform(dataset)
 
-    return preprocess_dataset
+    return preprocess_dataset, params_prep
+
 
 
 
