f9792d00c411158e8a08565cbcfe95f0c29eab7b
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-10 22:14:05 +0300

    Update data.py

--- src/capstone_project/data.py
+++ src/capstone_project/data.py
@@ -7,9 +7,8 @@ import click
 
 # Read the datasets from the files
 
-def get_data(path: Path, file_name = 'train.csv') -> Tuple[pd.DataFrame, pd.Series]:
-    file_path = os.path.join(path, file_name)
-    dataset = pd.read_csv(file_path)
+def get_data(path: Path) -> Tuple[pd.DataFrame, pd.Series]:
+    dataset = pd.read_csv(path)
 
     click.echo('Dataset size: {0}.'.format(dataset.shape))
 
